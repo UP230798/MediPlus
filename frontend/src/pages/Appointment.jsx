c1872bb7a8411c1efc00593c7a6d6083e051e564
David Duarte Delgado <up230798@alumnos.upa.edu.mx>
2025-07-18 17:42:33 -0600

    appointment.jsx fix

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,3 +1,4 @@
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
