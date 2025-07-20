600118630df3cfade7e0fe7c7230a64a8ced4c62
David Duarte Delgado <up230798@alumnos.upa.edu.mx>
2025-07-19 22:30:33 -0600

    pending issues with mongo connection

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,66 @@
+import validator from 'validator'
+import bycrypt from 'bcrypt'
+import { v2 as cloudinary } from "cloudinary"
+import doctorModel from "../models/doctorModel.js"
+
+
+
+// API para agregar médicos
+
+const addDoctor = async (req,res) => {
+
+    try {
+
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
+        const imageFile = req.file
+
+        // verificando la información para añadir médicos
+        if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
+            return res.json({success:false,message:"Información Incompleta"})
+        }
+
+        // validación de formato de email
+        if(!validator.isEmail(email)) {
+            return res.json({success:false,message:"Ingrese un Email válido"})
+        }
+
+        // validación de contraseña fuerte
+        if(password.lenght < 8) {
+            return res.json({success:false,message:"Por favor ingrese una contraseña más larga"})
+        }
+
+        // hash del password
+        const salt = await bycrypt.genSalt(10)
+        const hashedPassword = await bycrypt.hash(password, salt)
+
+        //Subir imagen a cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"})
+        const imageUrl = imageUpload.secure_url
+
+        const doctorData = {
+            name,
+            email,
+            image:imageUrl,
+            password:hashedPassword,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address:JSON.parse(address),
+            date:Date.now()
+        }
+
+        const newDoctor = new doctorModel(doctorData)
+        await newDoctor.save()
+
+        res.json({success:true,message:"Médico Añadido"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+
+export {addDoctor}
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,32 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 // app config
 
 const app = express()
 const port = process.env.PORT || 4000
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+connectDB()
+connectCloudinary()
+
 // middlewares
 app.use(express.json())
 app.use(cors())
 
 // api endpoints
+app.use('/api/admin',adminRouter)
+// localhost:4000/api/admin/add-doctor
+
 
 app.get('/',(req,res)=>{
     res.send('API WORKING Great')
